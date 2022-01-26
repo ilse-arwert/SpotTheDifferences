c2494455870fbcc6999be9a746065fa3d3e96a19
Ilse Arwert <ilsearwert@gmail.com>
2022-01-26 03:46:30 +0100

    moved things out of dist

---
+++ arrays.php
@@ -0,0 +1,10 @@
+<?php
+    //initialize the arrays
+    $letterImagesSmall = array();
+    $letterImagesLarge = array();
+
+    $letterImagesSmall[0] = '/data/a1.png';
+    $letterImagesLarge[0] = '/data/a2.png';
+    $letterImagesSmall[1] = '/data/b1.jpg';
+    $letterImagesLarge[1] = '/data/b2.jpg';
+?>
