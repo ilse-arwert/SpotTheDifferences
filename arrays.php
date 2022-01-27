4044354b0acea76b45f4f82e49e7440878ff915a
Ilse Arwert <ilsearwert@gmail.com>
2022-01-27 20:30:00 +0100

    correct dataset; cosmetic changes to interface

--- arrays.php
+++ arrays.php
@@ -2,9 +2,14 @@
     //initialize the arrays
     $letterImagesSmall = array();
     $letterImagesLarge = array();
+    $lettersUsed = array("a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t");
 
-    $letterImagesSmall[0] = '/data/a1.png';
-    $letterImagesLarge[0] = '/data/a2.png';
-    $letterImagesSmall[1] = '/data/b1.jpg';
-    $letterImagesLarge[1] = '/data/b2.jpg';
+    foreach($lettersUsed as $letter) {
+        $letterImagesSmall[] = '/data/small/' . $letter . '.jpg';
+        $letterImagesLarge[] = '/data/big/' . $letter . '.jpg';
+    }
+
+    foreach($letterImagesLarge as $l) {
+        echo $l . "\n";
+    }
 ?>
